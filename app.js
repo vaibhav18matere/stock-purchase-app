717a13f660c58349f83a85476080d999a1822397
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-29 16:25:34 +0530

    built profit and loss logic

--- app.js
+++ app.js
@@ -6,3 +6,22 @@ const currentPrice = document.querySelector("#current-price");
 const submitButton = document.querySelector("#submit-btn");
 const outputBox = document.querySelector("#output-box");
 
+calculateProfitAndLoss = (initial, quantity, current) => {
+    if (initial < current) {
+        //profit logic
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
+        console.log(`The profit is ${profit} and profit percentage is ${profitPercentage}% 🤩`);
+
+    } else if (initial > current) {
+        //loss logic
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+        console.log(`The loss is ${loss} and the loss percentage is ${lossPercentage}% 😌`);
+
+    } else {
+        console.log("No pain, No gain.. and No gain so No pain !😃")
+    }
+}
+
+submitButton.addEventListener("click", calculateProfitAndLoss);
