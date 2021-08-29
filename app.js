6a83dda209ca6553d5978b0c1bf57a42d50ce6e8
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-29 16:25:06 +0530

    took input in js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Let's Start !");
+
+const initialPrice = document.querySelector("#initial-price");
+const stockQuantity = document.querySelector("#stock-quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitButton = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output-box");
+
