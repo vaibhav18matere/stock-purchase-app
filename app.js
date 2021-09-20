58d6c682421350edc007ad8d284182db16d71886
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-09-21 00:05:01 +0530

    feat : added loading image feature

--- app.js
+++ app.js
@@ -13,30 +13,36 @@ submitHandler = () => {
     calculateProfitAndLoss(iPrice, qty, cPrice);
 }
 
+
 calculateProfitAndLoss = (initial, quantity, current) => {
+    loadingImg.style.display = "block";
+    outputBox.innerHTML = "";
+    setTimeout(() => {
+        loadingImg.style.display = "none";
+        if (initial <= 0 || quantity <= 0 || current <= 0) {
+            return outputBox.innerText = "Please enter valid inputs";
+        }
 
-    if (initial <= 0 || quantity <= 0 || current <= 0) {
-        return outputBox.innerText = "Please enter valid inputs";
-    }
+        else if (initial < current) {
+            //profit logic
+            var profit = (current - initial) * quantity;
+            var profitPercentage = (profit / (initial * quantity)) * 100;
+            showOutput(`The profit is ${profit} and profit percentage is ${profitPercentage.toFixed(2)}% 💰🤩`);
+            outputBox.style.backgroundColor = 'green';
 
-    else if (initial < current) {
-        //profit logic
-        var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / (initial * quantity)) * 100;
-        showOutput(`The profit is ${profit} and profit percentage is ${profitPercentage.toFixed(2)}% 💰🤩`);
-        outputBox.style.backgroundColor = 'green';
+        } else if (initial > current) {
+            //loss logic
+            var loss = (initial - current) * quantity;
+            var lossPercentage = (loss / (initial * quantity)) * 100;
+            showOutput(`The loss is ${loss} and the loss percentage is ${lossPercentage.toFixed(2)}% 😌`);
+            outputBox.style.backgroundColor = 'red';
 
-    } else if (initial > current) {
-        //loss logic
-        var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / (initial * quantity)) * 100;
-        showOutput(`The loss is ${loss} and the loss percentage is ${lossPercentage.toFixed(2)}% 😌`);
-        outputBox.style.backgroundColor = 'red';
+        } else {
+            showOutput("You are on 'break-even' point! Means, No profit or no loss yet! ");
+            outputBox.style.backgroundColor = 'white';
+        }
+    }, 2000);
 
-    } else {
-        showOutput("You are on 'break-even' point! Means, No profit or no loss yet! ");
-        outputBox.style.backgroundColor = 'white';
-    }
 }
 
 showOutput = (message) => {
